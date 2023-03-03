f81962e5d0ce1f45430d897988d0919bf9329884
dae-ne <rafalcz29@gmail.com>
2023-03-03 20:02:19 +0100

    Rename variables

--- INT001-calendar-to-todoist/src/Infrastructure/Authorization/AuthService.cs
+++ INT001-calendar-to-todoist/src/Infrastructure/Authorization/AuthService.cs
@@ -39,34 +39,34 @@ internal sealed class AuthService : IAuthService
     
     public async Task RefreshTokenAsync(CancellationToken cancellationToken)
     {
-        // TODO: move to configuration
-        const string partitionKey = "1CB3204D-220B-4A60-A267-30DA61BD2C5C";
-        const string rowKey = "06A902A7-16F6-4F1E-8544-0B215B056594";
-        
-        var response = await _tableClient.GetEntityAsync<AuthTokenEntity>(partitionKey, rowKey, cancellationToken: cancellationToken);
+        var authTableResponse = await _tableClient.GetEntityAsync<AuthTokenEntity>(
+            _authTableOptions.PartitionKey,
+            _authTableOptions.RowKey,
+            cancellationToken: cancellationToken);
         
         // TODO: handle response status code
         
         var data = new KeyValuePair<string, string>[]
         {
-            new("client_id", ""),
-            new("client_secret", ""),
-            new("refresh_token", response.Value.RefreshToken),
+            new("client_id", _googleOptions.ClientId),
+            new("client_secret", _googleOptions.ClientSecret),
+            new("refresh_token", authTableResponse.Value.RefreshToken),
             new("grant_type", "refresh_token")
         };
         
         var refreshRequest = new FormUrlEncodedContent(data);
-        var response2 = await _httpClient.PostAsync("token", refreshRequest, cancellationToken);
+        var googleResponse = await _httpClient.PostAsync("token", refreshRequest, cancellationToken);
         // // TODO: handle response status code
-        var content = await response2.Content.ReadAsStringAsync(cancellationToken);
+        var content = await googleResponse.Content.ReadAsStringAsync(cancellationToken);
         var token = JsonConvert.DeserializeObject<GoogleAuthResponseModel>(content);
         
         // TODO: if token is null throw exception
         
-        response.Value.AccessToken = token.AccessToken;
-        var xd = await _tableClient.UpdateEntityAsync(
-            response.Value,
-            response.Value.ETag,
+        authTableResponse.Value.AccessToken = token.AccessToken;
+        
+        var tokenUpdateResponse = await _tableClient.UpdateEntityAsync(
+            authTableResponse.Value,
+            authTableResponse.Value.ETag,
             TableUpdateMode.Replace,
             cancellationToken);
         
